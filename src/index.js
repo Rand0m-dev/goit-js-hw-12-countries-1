38a2f60d065384c141f8ab104520182eba5463d9
Niko Yea <nikoyea14@gmail.com>
2021-05-23 23:51:45 +0200

    create hw

---
+++ src/index.js
@@ -0,0 +1,81 @@
+import { alert, notice, info, success, error } from '@pnotify/core';
+import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/core/dist/BrightTheme.css";
+import './sass/main.scss';
+
+import fetchCountries from './js/fetchCountries';
+import countryInfoTpl from './partials/countryInfo.hbs';
+import countriesListTpl from './partials/countriesList.hbs';
+
+const _ = require('lodash');
+
+const refs = {
+  inputEl: document.querySelector('.form-input'),
+  resultContainer: document.querySelector('.for-result')
+}
+
+refs.inputEl.addEventListener('input', _.debounce(onInput, 500));
+
+function onInput(e) {
+  const inputValue = e.target.value;
+
+  if (inputValue === '') {
+    notice({
+      text: 'Please type country name',
+      type: 'notice',
+      sticker: false,
+      maxTextHeight: null,
+      delay: 3000,
+    })
+    return;
+  }
+
+  refs.resultContainer.innerHTML = '';
+
+  fetchCountries(inputValue).then(createResult);
+}
+
+function createResult(countryArr) {
+  const listLength = countryArr.length;
+
+  const { status } = countryArr;
+
+  if (status === 404) {
+    return error({
+      text: 'Country not fount. Try agin',
+      type: 'error',
+      sticker: false,
+      maxTextHeight: null,
+      delay: 3000,
+  })
+  }
+
+  if (listLength === 1) {
+    return countryInfoMarkup(countryArr);
+  }
+
+  if (listLength >= 2 && listLength <= 10) {
+    return listOfCountryMarkup(countryArr);
+  }
+
+  if (listLength > 10) {
+    alert({
+      text: 'Too many matches found. Please enter a more specific query!',
+      type: 'info',
+      sticker: false,
+      maxTextHeight: null,
+      delay: 3000,
+    })
+  }
+}
+
+function countryInfoMarkup(countryArr) {
+  const fetchCountry = countryArr.map(countryInfoTpl).join('');
+  refs.resultContainer.insertAdjacentHTML('beforeend', fetchCountry);
+}
+
+function listOfCountryMarkup(countryArr) {
+  const fetchList = countriesListTpl(countryArr);
+  refs.resultContainer.innerHTML = fetchList;
+}
+
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,7 @@
+export default function fetchCountries(searchQuery) {
+  const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
+
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => data);
+}
